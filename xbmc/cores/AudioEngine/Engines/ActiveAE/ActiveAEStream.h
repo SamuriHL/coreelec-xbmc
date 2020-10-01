9372529a4e644162ef58e94dc875d415e5a6d41b
fritsch <Peter.Fruehberger@gmail.com>
2020-10-01 21:32:38 +0200

    ActiveAE: Include LFE mixing possibility
    We have user without AVR running fullrange speakers. They want the bass of
    a 5.1 or 7.1 layout being mixed into FL and FR. This new setting allows
    to do so.

--- xbmc/cores/AudioEngine/Engines/ActiveAE/ActiveAEStream.h
+++ xbmc/cores/AudioEngine/Engines/ActiveAE/ActiveAEStream.h
@@ -95,12 +95,16 @@ class CActiveAEStreamBuffers
 public:
   CActiveAEStreamBuffers(const AEAudioFormat& inputFormat, const AEAudioFormat& outputFormat, AEQuality quality);
   virtual ~CActiveAEStreamBuffers();
-  bool Create(unsigned int totaltime, bool remap, bool upmix, bool normalize = true) const;
+  bool Create(
+      unsigned int totaltime, bool remap, bool upmix, bool normalize = true, float sublevel = 0.0f);
   void SetExtraData(int profile, enum AVMatrixEncoding matrix_encoding, enum AVAudioServiceType audio_service_type);
   bool ProcessBuffers();
-  void ConfigureResampler(bool normalizelevels, bool stereoupmix, AEQuality quality) const;
-  bool HasInputLevel(int level) const;
-  float GetDelay() const;
+  void ConfigureResampler(bool normalizelevels,
+                          bool stereoupmix,
+                          AEQuality quality,
+                          float sublevel);
+  bool HasInputLevel(int level);
+  float GetDelay();
   void Flush();
   void SetDrain(bool drain) const;
   bool IsDrained() const;
--- xbmc/cores/paplayer/VideoPlayerCodec.cpp
+++ xbmc/cores/paplayer/VideoPlayerCodec.cpp
@@ -258,7 +258,8 @@ bool VideoPlayerCodec::Init(const CFileItem &file, unsigned int filecache)
                        M_SQRT1_2,
                        nullptr,
                        AE_QUALITY_UNKNOWN,
-                       false);
+                       false,
+                       0.0f);
 
     m_planes = AE_IS_PLANAR(m_srcFormat.m_dataFormat) ? m_channels : 1;
     m_format = m_srcFormat;
