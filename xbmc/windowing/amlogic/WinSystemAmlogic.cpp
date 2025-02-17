fbc241675372c991b6deeb753708eacb0e8b9553
Portisch <hugo.portisch@yahoo.de>
2025-02-17 11:04:31 +0100

    AMLUtils: set frame buffer only on mode change fb screen size must be set before mode change so new mode becomes valid

--- xbmc/windowing/amlogic/WinSystemAmlogic.cpp
+++ xbmc/windowing/amlogic/WinSystemAmlogic.cpp
@@ -258,9 +258,6 @@ bool CWinSystemAmlogic::InitWindowSystem()
   RETRO::CRPProcessInfoAmlogic::RegisterRendererFactory(new RETRO::CRendererFactoryOpenGLES);
   CRendererAML::Register();
 
-  if (aml_get_cpufamily_id() <= AML_GXL)
-    aml_set_framebuffer_resolution(1920, 1080, m_framebuffer_name);
-
   auto setting = settings->GetSetting(CSettings::SETTING_VIDEOPLAYER_USEDISPLAYASCLOCK);
   if (setting)
   {
