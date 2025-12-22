c09923c8158be82b78cf356b08a51224297c882f
cpm <162544069+cpm-code@users.noreply.github.com>
2025-12-22 20:07:27 +0800

    AdvancedSettings: Add back audio passthrough max off sync duration

--- xbmc/settings/AdvancedSettings.h
+++ xbmc/settings/AdvancedSettings.h
@@ -169,6 +169,7 @@ class CAdvancedSettings : public ISettingCallback, public ISettingsHandler
     int m_videoIgnoreSecondsAtStart;
     float m_videoIgnorePercentAtEnd;
     float m_audioApplyDrc;
+    unsigned int m_maxPassthroughOffSyncDuration = 10; // when off by this value then adjust
     unsigned int m_audioAddPacketUnlockTime = 1000;
     bool m_AllowMultiChannelFloat = false; // Android only switch to be removed in v22
     bool m_superviseAudioDelay = false; // Android only to correct broken audio firmwares
