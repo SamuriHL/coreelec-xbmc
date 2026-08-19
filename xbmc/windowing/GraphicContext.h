chucky88/xbmc

GraphicContext: directly set stereo mode and do not trigger resolution update Resolution update is done by whitelist.

xbmc/windowing/GraphicContext.h
@@ -132,7 +132,7 @@ public:
   void SetCameraPosition(const CPoint &camera);
   void SetStereoView(RENDER_STEREO_VIEW view);
   RENDER_STEREO_VIEW GetStereoView()  { return m_stereoView; }
-  void SetStereoMode(RENDER_STEREO_MODE mode) { m_nextStereoMode = mode; }
+  void SetStereoMode(RENDER_STEREO_MODE mode) { m_stereoMode = mode; }
   RENDER_STEREO_MODE GetStereoMode()  { return m_stereoMode; }
   void SetHDRType(StreamHdrType hdrType)  { m_hdrType = hdrType; }
   StreamHdrType GetHDRType()  { return m_hdrType; }
@@ -236,7 +236,6 @@ protected:
   std::stack<UITransform> m_transforms;
   RENDER_STEREO_VIEW m_stereoView = RENDER_STEREO_VIEW_OFF;
   RENDER_STEREO_MODE m_stereoMode = RENDER_STEREO_MODE_OFF;
-  RENDER_STEREO_MODE m_nextStereoMode = RENDER_STEREO_MODE_OFF;
   StreamHdrType m_hdrType;
 
   bool m_isTransferPQ{false};

SamuriHL/coreelec-xbmc

xbmc/windowing/GraphicContext.h
@@ -159,7 +159,7 @@ public:
   void SetCameraPosition(const CPoint &camera);
   void SetStereoView(RenderStereoView view);
   RenderStereoView GetStereoView() { return m_stereoView; }
-  void SetStereoMode(RenderStereoMode mode) { m_nextStereoMode = mode; }
+  void SetStereoMode(RenderStereoMode mode) { m_stereoMode = mode; }
   RenderStereoMode GetStereoMode() { return m_stereoMode; }
   void SetHDRType(StreamHdrType hdrType)  { m_hdrType = hdrType; }
   StreamHdrType GetHDRType()  { return m_hdrType; }
@@ -273,7 +273,6 @@ protected:
   std::stack<UITransform, std::vector<UITransform>> m_transforms;
   RenderStereoView m_stereoView = RenderStereoView::OFF;
   RenderStereoMode m_stereoMode = RenderStereoMode::OFF;
-  RenderStereoMode m_nextStereoMode = RenderStereoMode::OFF;
   StreamHdrType m_hdrType;
 
   bool m_isTransferPQ{false};