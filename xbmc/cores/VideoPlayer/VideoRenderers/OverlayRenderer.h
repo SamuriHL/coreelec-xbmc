52e58d2514e6aa946bde62a0c20d041a1448e3af
SamuriHL <samurihl@msn.com>
2026-09-23 16:20:57 -0400

    Revert "DIAG: log HDR overlay draw rects (temporary)"
    This reverts commit 6869d5484bdd929d2ef273a812133e4644378f20.

--- xbmc/cores/VideoPlayer/VideoRenderers/OverlayRenderer.h
+++ xbmc/cores/VideoPlayer/VideoRenderers/OverlayRenderer.h
@@ -97,8 +97,6 @@ namespace OVERLAY {
     bool m_isPGS{false};
     bool m_isColoredPGS{false};
     bool m_pgsPaletteConvertedToSrgb{false};
-    int m_texWidthDiag{0}; // DIAG
-    int m_texHeightDiag{0}; // DIAG
 
     int m_3dSubtitleDepth{0};
     bool m_pgsSubtitle{false};
